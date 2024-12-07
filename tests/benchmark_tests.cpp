ee1563759dacb0a25a83c644bc145537e92936c8
AlexandrShadrukhin <114620271+AlexandrShadrukhin@users.noreply.github.com>
2024-12-07 19:25:50 +0300

    Add files via upload

---
+++ tests/benchmark_tests.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <cstdlib>
+#include <filesystem>
+#include <string>
+
+TEST(DedupBenchmarkTest, RunsSuccessfully) {
+    int ret = std::system("./dedup_benchmark 1000 10000");
+    EXPECT_EQ(ret, 0) << "dedup_benchmark-app не завершился успешно";
+}
+
+TEST(SearchNameBenchmarkTest, RunsSuccessfully) {
+    std::ostringstream commandStream;
+    commandStream << "./search_name_benchmark 10 ./ build.ninja";
+    std::string command = commandStream.str();
+    int ret = std::system(command.c_str());
+    EXPECT_EQ(ret, 0) << "search_name_benchmark-app не завершился успешно";
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
